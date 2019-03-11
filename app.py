06ce6aac68b4f848a0f2a256e7a48004c2e63840
Nathan Pillai <natepill@gmail.com>
2019-03-11 14:14:20 -0700

    ADDED date-key-mapper and REMOVED most todo and note comments

--- app.py
+++ app.py
@@ -2,35 +2,28 @@ import csv
 import json
 from collections import OrderedDict
 
-
+'''This API is a microservice used for Make School's Unconference Slack Bot reminder utility'''
 '''Reads in the entire DOWNLOADED Unconference CSV file, converts it to a usable JSON array and returns it'''
 '''The converted JSON is also written to a json file for immidiete reference'''
 
 
-
-#TODO: If I can remap the headers of the json objects then I can
-
-# TODO: Remapping headers will look like:
-    # Iterate over each json object,
-    # Use the date key, value from each json object and make the current json object the value of a new json object
-    # The new json object will by "<actual date>": {<object with key,values of who is speaking and their topic>}
-    # NOTE: I can probably remove the date key,value from the original object
-    # NOTE: The result from this remapping will be that we can look up who is
-
-
-#NOTE: If I remap the headers then I shouldn't store the json objects into an array (maybe pass in a json array into a function)
-
-def remap(json_arry):
-
-
-
-# TODO: Create a function that writes current csv lines to a json file and returns the json object itselfself.
-
 # NOTE: Do I want to have the entire csv already read into the API and reference that? Probably, cause I'm not currently making requests to download the Google Sheet
+# NOTE: This script WILL THROW UNHANDLED ERRORS if a row doesn't have a Date value ---> TODO: Add in error handling empty Date cells if even needed
 
 
 
-# TODO: Need to wrap some blocks in try if json_file doesnt exist or if blocks
+def map_date_keys(json_array):
+    '''Maps over json array and returns a dictionary with key,value = "<Date>": {<object with key,values of who is speaking and their topic>}'''
+
+    schedule_by_date = dict()
+
+    for json_object in json_array:
+        schedule_by_date[json_object["Date"]] = json_object
+        del json_object['Date']
+
+    return schedule_by_date
+
+
 def convert_to_json(csv_file, fieldnames, json_file = None):
     '''Takes in csv file name, fieldnames headers, and optional jsonfile name to write the results to'''
     '''Will read csv file, convert it to an array of json objects, and return it'''
@@ -49,7 +42,6 @@ def convert_to_json(csv_file, fieldnames, json_file = None):
             json.dump(row, jsonfile)
             jsonfile.write('\n')
 
-    print(json_array[0])
     return json_array
 
 def main():
@@ -57,9 +49,11 @@ def main():
     fieldnames = ("Date","Speaker 1","Topic 1","Speaker 2","Topic 2","Speaker 3","Topic 3","Speaker 4","Topic 4")
     jsonfile = 'file.json'
 
-    schedule_json = convert_to_json(csv_file, fieldnames, json_file = 'file.json')
+    schedule_json_array = convert_to_json(csv_file, fieldnames, json_file = 'file.json')
 
-    # print(schedule_json)
+    final_object = map_date_keys(schedule_json_array)
+
+    print(final_object)
 
 
 
